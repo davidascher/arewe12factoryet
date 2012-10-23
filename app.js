ec93d4938652a1c7fc9abfb98b00262dced43916
David Ascher <david.ascher@gmail.com>
2012-10-23 16:41:49 -0700

    checkpoint

--- app.js
+++ app.js
@@ -6,18 +6,47 @@ var repoData = {};
 var repos = ['davidascher/arewe12factoryet'];
 
 for (var i = repos.length - 1; i >= 0; i--) {
-	var reponame = repos[i];
-  var arewethereurl = 'https://raw.github.com/' + reponame + '/master/.arewethereyet.json';
-  console.log('arewethereurl: ', arewethereurl);
-	$.get(arewethereurl, function(data) {
-    $.each(data, function(key, val) {
-      console.log(key, val);
-      items.push('<li id="' + key + '">' + val + '</li>');
-    });
+	var repoparts = repos[i].split('/');
+  var username = repoparts[0];
+  var reponame = repoparts[1];
 
-    $('<ul/>', {
-      'class': 'my-new-list',
-      html: items.join('')
-    }).appendTo('body');
+
+  var user = new Gh3.User(username)
+  var repo = new Gh3.Repository(reponame, user);
+  var myfile;
+  repo.fetch(function (err, res) {
+    if(err) { throw "outch ..." }
+
+    repo.fetchBranches(function (err, res) {
+      if(err) { throw "outch ..." }
+
+      var master = repo.getBranchByName("master");
+      master.fetchContents(function (err, res) {
+        console.log("master: ", master);
+        myfile = master.getFileByName(".arewethereyet.json");
+        console.log("myfile:", myfile);
+        myfile.fetchContent(function (err, res) {
+          if(err) { throw "outch ..." }
+          var raw = myfile.getRawContent();
+        console.log("raw = ", raw);
+          var data = JSON.parse(raw);
+          console.log(data);
+        });
+      });
+    })
   });
-};
+}
+
+ //  var arewethereurl = 'https://raw.github.com/' + reponame + '/master/.arewethereyet.json';
+ //  console.log('arewethereurl: ', arewethereurl);
+	// $.get(arewethereurl, function(data) {
+ //    $.each(data, function(key, val) {
+ //      console.log(key, val);
+ //      items.push('<li id="' + key + '">' + val + '</li>');
+ //    });
+
+ //    $('<ul/>', {
+ //      'class': 'my-new-list',
+ //      html: items.join('')
+ //    }).appendTo('body');
+ //  });
